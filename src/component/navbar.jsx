fbe30467c06e02c7ccbc05df0fdf26391b308631
NajoroRabiaza <hei.najoro.2@gmail.com>
2025-03-06 14:04:33 +0300

    commit lavareny

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -13,12 +13,31 @@ function customerNavbar() {
         <>
            <Navbar className="Barre_Menu">
          <Container>
-           <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+           <Navbar.Brand as={Link} to="/"><img
+           src="src\image\ikaly.png"
+           alt="logo"
+           width="30"
+           height="30"
+           className="logonav"
+           />
+           </Navbar.Brand>
            <Navbar.Toggle aria-controls="basic-navbar-nav" />
            <Navbar.Collapse id="basic-navbar-nav">
              <Nav className="ms-auto">
+<<<<<<< HEAD
                <Nav.Link  className=" Navbar_text text-center me-5">Home</Nav.Link>
                <Nav.Link href="#link">Lien</Nav.Link>
+=======
+               <Nav.Link href="#link">
+                <img
+                src="src\image\panier.png"
+                alt="panier"
+                width="30"
+                height="30"
+                className="panier"
+                />
+               </Nav.Link>
+>>>>>>> d32a42e... nanamboatra design VICMORT
              </Nav>
            </Navbar.Collapse>
          </Container>
