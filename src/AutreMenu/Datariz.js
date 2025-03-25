e1e4c205245974c9f31782cd184cf108bcbf18ea
Danilochristohe <danilobelalahy6@gmail.com>
2025-03-25 15:35:51 +0100

    image data js

---
+++ src/AutreMenu/Datariz.js
@@ -0,0 +1,27 @@
+const rizlist = [
+    {
+      id: 1,
+      nom: "hamburger",
+      description: "lorem ipsum dolor sit amet",
+      prix: 20000,
+      img: "/src/image/Burger.png",
+    },
+    {
+      id: 2,
+      nom: "Pizza",
+      description: "lorem ipsum dolor sit amet",
+      prix: 20000,
+      img: "/src/image/Pizzaimage.jpg",
+    },
+    {
+      id: 3,
+      nom: "Soupe Legume",
+      description: "lorem ipsum dolor sit amet",
+      prix: 20000,
+      img: "/src/image/imgCrudité.jpeg",
+    },
+    
+    
+ ];
+
+export default rizlist;
