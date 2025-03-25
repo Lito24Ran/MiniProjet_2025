df84645300a9bec8ea3694557812eb235f188b96
Victorio178 <victoriorandriamanalina@gmail.com>
2025-03-25 13:09:10 -0700

    nanova responsive vic

--- src/page/Panier.jsx
+++ src/page/Panier.jsx
@@ -1,6 +1,7 @@
 import { useContext} from "react";
 import { CartContext } from "../context/CartContext";
 import Navbar from '../component/navbar';
+import './panier.css'
 
 
 function Panier () {
@@ -8,24 +9,24 @@ function Panier () {
 
     return ( <>
                 <Navbar size={cart.length} /> 
-            <div>
+            <div className="panier_container"> 
                 <h1>Votre panier {cart.length === 0 ? "est vide" : "contient des articles"}.</h1>
                 <article>
         {
-            cart?.map((item)=>(
+            cart.map((item)=>(
                 <div className="cart_box" key={item.id}>
                     <div className="cart_img">
-                        <img src={item.img} />
-                        <p>{item.title}</p>
+                        <img src={item.img} alt={item.title} className="pan_image"/>
+                        <h3 className="name">{item.nom}</h3>
                     </div>
-                    <div>
+                    <div className="quantite">
                         <button> - </button>
                         <button>{item.amount}</button>
                         <button> + </button>
                     </div>
                     <div>
-                        <span>{item.price}</span>
-                        <button>Remove</button>
+                        <span className="prix_pan">{item.prix}Ar</span>
+                        <img src="src\image\remove.png" alt="remove" className="remove" />
                     </div>
                 </div>
             ))}
