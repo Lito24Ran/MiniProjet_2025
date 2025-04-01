cd00b66f62c012d3cfe2d6e35f5b6298c36cd3fd
randrianaivosonazaria@gmail.com <randrianaivosonazaria@gmail.com>
2025-04-01 14:49:02 +0300

    niova le sary tao @ composant autre reny

--- src/AutreMenu/Databoisson.js
+++ src/AutreMenu/Databoisson.js
@@ -1,25 +1,26 @@
 const boissonlist = [
     {
       id: 1,
-      nom: "hamburger",
-      description: "lorem ipsum dolor sit amet",
-      prix: 20000,
-      img: "/src/image/SOUPELITO.jpg",
+      nom: "Jus orange",
+      description: "orange, eau, citron",
+      prix: 10000,
+      img: "src/image/boisson1.jpg",
     },
     {
       id: 2,
-      nom: "Pizza",
-      description: "lorem ipsum dolor sit amet",
-      prix: 20000,
-      img: "/src/image/Pizzaimage.jpg",
+      nom: "caffe",
+      description: "caffe, lait",
+      prix: 12000,
+      img: "src/image/coffe.jpg",
     },
     {
       id: 3,
-      nom: "Soupe Legume",
-      description: "lorem ipsum dolor sit amet",
+      nom: "gallantry whiskey",
+      description: "le meilleur des whiskey ",
       prix: 20000,
-      img: "/src/image/imgCrudité.jpeg",
+      img: "src/image/whiskey.jpg",
     },
+    
     
     
  ];
