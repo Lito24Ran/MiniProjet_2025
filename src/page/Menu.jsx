11bd59b8d8c5b57bdb9c8c426c9d3df013492d19
Victorio178 <victoriorandriamanalina@gmail.com>
2025-04-03 20:45:35 -0700

    last commit

--- src/page/Menu.jsx
+++ src/page/Menu.jsx
@@ -4,7 +4,7 @@ import Navbar from "../component/navbar";
 import  Row  from "react-bootstrap/Row";
 import  Col  from "react-bootstrap/Col";
 import Stack from 'react-bootstrap/Stack';
-import Cards from '../component/Card';
+import Cartes from "../component/cartes";
 import { createContext, useContext, useState} from "react";
 import { CartContext } from "../context/CartContext";
 import list from './DataMenu'
@@ -17,11 +17,15 @@ function  Menu (){
         <>
             <Navbar size={cart.length} />
             <section>
+            <img src="src\image\font.png" alt="font" className="fonts" />
+           <div className='menus'>Menu du jour</div> 
+           <div className="All_Cards">
         {
             list.map((item)=>(
-                <Cards item={item} key={item.id} handleClick={handleClick} />
+                <Cartes item={item} key={item.id} handleClick={handleClick} />
             ))
         }
+        </div>
     </section>
         </>
     )
