de5c416d9dea3c64d2a0ec1ac2ed5d39a8804c47
Victorio178 <victoriorandriamanalina@gmail.com>
2025-06-18 14:36:14 -0700

    first 2e

---
+++ src/admin/components/TopNav/TopNav.jsx
@@ -0,0 +1,29 @@
+ 
+
+import { Link } from "react-router-dom";
+import profileImg from "../../assets/images/profile-02.png";
+import "./top-nav.css";
+
+const TopNav = () => {
+  return (
+    <div className="top__nav">
+      <div className="top__nav-wrapper">
+        <div className="search__box">
+          <input type="text" placeholder="search or type" />
+          <span>
+            <i className="ri-search-line"></i>
+          </span>
+        </div>
+        <div className="top__nav-right">
+          <div className="profile">
+            <Link to="">
+              <img src={profileImg} alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
