8d3a21aa402294411753f45b941706caafb2cef0
NajoroRabiaza <hei.najoro.2@gmail.com>
2025-06-19 20:09:56 +0300

    feat: mise des images dans mongo et fetcher les data depuis mongo vers home

--- backend/router/produits.js
+++ backend/router/produits.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const { getProduits } = require("../controleurs/produitControleur");
+const { getProduits, addProduit } = require("../controleurs/produitControleur");
 
+// Configuration Multer pour inserer les images dans mongo et en creer un lien
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+});
+const upload = multer({ storage });
+
+// Routes
 router.get("/", getProduits);
+router.post("/", upload.single("img"), addProduit); // ajout avec image
 
 module.exports = router;
