016a9875398291ef97ed01265ff1d8246c4ce7f1
Victorio178 <victoriorandriamanalina@gmail.com>
2025-06-20 09:01:58 -0700

    interface

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,36 +4,41 @@ import icon_Facebook from "../image/icon_facebook.png";
 import icon_mail from "../image/icon_mail.png";
 import icon_lieu from "../image/icon_lieu.png";
 import icon_contact from "../image/icon_contact.png";
-import logo from "../image/logo.png";
+import logo from "../image/kalico.png";
 
-function Foot(){
-
-    return(
-        <footer className="StyleFooter">
-            <div className="footer_3D">
-              <img src={logo} alt="logo" className="logo_ftr" />
-              <h3 className="contact">Contactez Nous:</h3>
-                <h3>
-                  <div className="item_ftr">
-                    <a href="https://www.facebook.com/HaiRunUniversity">
-                      <img src={icon_Facebook}  className="icon_ftr" />
-                             </a>
-                    <a href="contact@grande-ecole-it.com">
-                      <img src={icon_mail}  className="icon_ftr" />
-                              </a>
-                    <a href="https://maps.app.goo.gl/gaSQXBQ5bQrMGUCS8">
-                      <img src={icon_lieu}  className="icon_ftr" />
-                              </a>
-                    <a href="#">
-                      <img src={icon_contact}  className="icon_ftr" />
-                    </a>
-                  </div>
-                </h3>
-            </div>
-            <div className="txt_ftr">
-                <h1>Developée par :<> Team<span>Five</span></></h1>
-            </div>
-        </footer>
-    );
+function Foot() {
+  return (
+    <footer className="StyleFooter">
+      <div className="footer_3D">
+        <img src={logo} alt="logo" className="logo_ftr" />
+        <h3 className="contact">Contactez Nous:</h3>
+        <h3>
+          <div className="item_ftr">
+            <a href="https://www.facebook.com/HaiRunUniversity">
+              <img src={icon_Facebook} className="icon_ftr" />
+            </a>
+            <a href="contact@grande-ecole-it.com">
+              <img src={icon_mail} className="icon_ftr" />
+            </a>
+            <a href="https://maps.app.goo.gl/gaSQXBQ5bQrMGUCS8">
+              <img src={icon_lieu} className="icon_ftr" />
+            </a>
+            <a href="#">
+              <img src={icon_contact} className="icon_ftr" />
+            </a>
+          </div>
+        </h3>
+      </div>
+      <div className="txt_ftr">
+        <h1>
+          Developée par :
+          <>
+            {" "}
+            Bug<span>409</span>
+          </>
+        </h1>
+      </div>
+    </footer>
+  );
 }
-export default Foot
+export default Foot;
