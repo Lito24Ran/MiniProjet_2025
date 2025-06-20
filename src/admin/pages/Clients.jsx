b527077cdcdb5ce0af14740784693251a64853e9
Najoro <128323908+JoroAmboara@users.noreply.github.com>
2025-06-20 09:58:33 +0300

    Update Clients.jsx

--- src/admin/pages/Clients.jsx
+++ src/admin/pages/Clients.jsx
@@ -1,35 +1,30 @@
 import { useEffect, useState } from 'react';
-import CustomTable from '../components/CustomTable';
+import CustomTable from '../components/CustomTable'; // ou Ant Design Table
 
 const columns = [
   { id: 'name', label: 'Nom', minWidth: 150 },
-  { id: 'email', label: 'Email', minWidth: 200 },
   { id: 'level', label: 'Niveau', minWidth: 100 },
+  { id: 'date', label: 'Date', minWidth: 150 }
 ];
 
 export default function Clients() {
   const [clients, setClients] = useState([]);
 
-      useEffect(() => {
-        const dataComparing = async () => {
-            try {
-                const datafetching = await fetch("http://localhost:1203/dataUser", {
-                    method: 'GET'
-                });
-                const donnefetch = await datafetching.json();
-                //console.log(donnefetch[0].name);
-                setData(donnefetch)
+  useEffect(() => {
+    fetch('http://localhost:1203/clients')
+      .then(res => res.json())
+      .then(data => {
+        if (Array.isArray(data)) setClients(data);
+        else console.error('Format inattendu', data);
+      })
+      .catch(err => console.error('Erreur fetch clients:', err));
+  }, []);
 
-            } catch (error) {
-                console.log("Une erreur c' est produit!");
-
-            }
-
-
-        }
-        dataComparing();
-        //console.log(data[0].email);
-    }, [data])
-
-  return <CustomTable columns={columns} rows={clients} uniqueKey="_id" />;
+  return (
+    <CustomTable
+      columns={columns}
+      rows={clients}
+      uniqueKey="_id"
+    />
+  );
 }
