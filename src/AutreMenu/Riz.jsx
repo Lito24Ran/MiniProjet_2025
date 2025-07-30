3bd2a998c10148420ef55cb0468f762280bb9eeb
NajoroRabiaza <hei.najoro.2@gmail.com>
2025-07-30 21:06:34 +0300

    chore(toast) : ajout de toast pour tout les cartes restant

--- src/AutreMenu/Riz.jsx
+++ src/AutreMenu/Riz.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, useContext } from "react";
 import Navbar from "../component/navbar";
 import { CartContext } from "../context/CartContext";
+import { useToast } from "../context/ToastContext";
 import Cartes from "../component/cartes";
 import Foot from "../component/footer";
 
 function Riz() {
   const { cart, handleClick } = useContext(CartContext);
+  const { showToast } = useToast();
   const [produits, setProduits] = useState([]);
 
   useEffect(() => {
@@ -30,7 +32,7 @@ function Riz() {
         <div className="menus">Riz</div>
         <div className="All_Cards">
           {produits.map((item) => (
-            <Cartes key={item._id} item={item} handleClick={handleClick} />
+            <Cartes handleClick={(item) => handleClick(item, showToast)} item={item} key={item._id} />
           ))}
         </div>
       </section>
